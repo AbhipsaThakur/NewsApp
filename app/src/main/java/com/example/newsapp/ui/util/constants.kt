38c4d4f9c56ea558670df92fb560e3efc8e88126
8260994511 <164469438+8260994511@users.noreply.github.com>
2024-11-15 02:56:44 +0530

    Update constants.kt

--- app/src/main/java/com/example/newsapp/ui/util/constants.kt
+++ app/src/main/java/com/example/newsapp/ui/util/constants.kt
@@ -1,4 +1,10 @@
 package com.example.newsapp.ui.util
 
 class constants {
+    companion object{
+        const val API_KEY = "fcb16e35e0cb287d939f21191d6ccd"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
 }
