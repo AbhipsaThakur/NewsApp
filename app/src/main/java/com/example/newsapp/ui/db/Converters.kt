950e3dd3dda1163daffaf6ca24a37f1dbd30e239
Mousumi77 <mousumimajhi377@gmail.com>
2024-11-15 10:38:15 +0530

    Create Converters.kt

---
+++ app/src/main/java/com/example/newsapp/ui/db/Converters.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.ui.db
+
+import androidx.room.TypeConverters
+import com.example.newsapp.ui.models.Source
+
+class Converters {
+
+    @TypeConverters
+    fun fromSource(source: Source):String{
+        return source.name
+    }
+
+    @TypeConverters
+    fun toSource(name: String):Source{
+        return Source(name,name)
+    }
+
+}
